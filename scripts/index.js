ab0d1effb54e9c0438465cb01362cc04fa9e1526
JaredGore <jaredg13100@gmail.com>
2025-10-02 18:25:48 -0600

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,32 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editprofilenameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+
+const editprofileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
+const editCardNameInput = document.querySelector("#post-title-input");
+const editCardLinkInput = document.querySelector("#post-link-input");
+
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+editProfileForm = editProfileModal.querySelector(".modal__form");
+newPostForm = newPostModal.querySelector(".modal__form");
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
+const addCardFormEl = document.querySelector("#add-card-form");
+
 editProfileBtn.addEventListener("click", function () {
+  editprofilenameInput.value = profileNameEl.textContent;
+  editprofileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 editProfileCloseBtn.addEventListener("click", function () {
@@ -13,9 +34,27 @@ editProfileCloseBtn.addEventListener("click", function () {
 });
 
 newPostBtn.addEventListener("click", function () {
+  editCardNameInput.value = addCardFormEl.textContent;
+  editCardLinkInput.value = addCardFormEl.textContent;
   newPostModal.classList.add("modal_is-opened");
 });
 
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleProfilesubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editprofilenameInput.value;
+  profileDescriptionEl.textContent = editprofileDescriptionInput.value;
+}
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  const titleInput = addCardFormEl.querySelector("#post-title-input");
+  const imageInput = addCardFormEl.querySelector("#post-image-input");
+  console.log(titleInput.value, imageInput.value);
+}
+
+addCardFormEl.addEventListener("submit");
+editProfileForm.addEventListener("submit");
